85162e9e2d13059273e436e719421a70a5b1953a
Sebastian Kreisel <kreisel.sebastian@gmail.com>
2015-04-05 23:56:42 +0200

    work on md parser

--- EditParser.hs
+++ EditParser.hs
@@ -12,39 +12,72 @@ import Text.Blaze.Html5.Attributes (href, rel, src, type_, class_, id)
 import Text.Blaze.Html.Renderer.Text (renderHtml)
 
 
-data Section = Headl L.Text | Parag L.Text deriving Show
-type Doc = [Section]
+-- Line-wise intermediate
+type Docum = [Section]
+data Section = Headl L.Text
+             | Parag L.Text
+             deriving Show
 
-parseEdit :: [L.Text] -> L.Text
-parseEdit content = renderHtml $ do
-  putHtml $ parseTitle (content !! 0)
-  putHtml $ parseCategories (content !! 1)
-  putHtml $ parseContent (content !! 2)
-  where putHtml (Just h) = h
-        putHtml Nothing = return ()
+-- The real stuff
+type Doc = [Block]
+data Block = Par [Ele]
+           | Hdl [Ele]
+           deriving Show
+data Ele = Plain L.Text
+         | Italic L.Text
+         deriving Show
 
-parseContent :: L.Text -> Maybe Html
-parseContent cont = case docToHtml $ parseToDoc cont of
-  Nothing -> Nothing
-  Just xs -> Just (toHtml $ xs)
+main = do
+  f <- readFile "test.txt"
+  --print $ parseToDoc $ L.pack f
+  print $ docToReal $ parseToDoc $ L.pack f
 
-docToHtml :: Doc -> Maybe [Html]
-docToHtml [] = Nothing
-docToHtml doc = Just $ map secToHtml doc
 
-secToHtml :: Section -> Html
-secToHtml (Headl t) = div (toHtml t) ! class_ "headline"
-secToHtml (Parag t) = div (toHtml t) ! class_ "parag"
+{-
+ Docum to the real stuff
+-}
+--bla :: Section -> Block
+docToReal :: Docum -> Doc
+docToReal doc = map bla doc
 
-parseToDoc :: L.Text -> Doc
+bla (Headl t) = Hdl $ kla t False []
+bla (Parag t) = Par $ kla t False []
+
+kla :: L.Text -> Bool -> [Ele] -> [Ele]
+kla "" _ acc = reverse acc
+kla t False []
+  | c == "*" && lookahead (L.tail t) = kla (L.tail t) True [Italic ""]
+  | otherwise = kla (L.tail t) False [Plain c]
+  where c = L.singleton $ L.head t
+kla t False (h : acc)
+  | c == "*" && lookahead (L.tail t) = kla (L.tail t) True
+                                       ((Italic "") : h : acc)
+  | otherwise = kla (L.tail t) False ((concatEle h c) : acc)
+  where c =  L.singleton $ L.head t
+kla t True (h : acc)
+  | c == "*" = kla (L.tail t) False ((Plain "") : h : acc)
+  | otherwise = kla (L.tail t) True ((concatEle h c) : acc)
+  where c = L.singleton $ L.head t
+
+lookahead = L.isInfixOf "*"
+concatEle (Plain t) tn = Plain $ L.append t tn
+concatEle (Italic t) tn = Italic $ L.append t tn
+
+
+{-
+ Text to Doc
+-}
+parseToDoc :: L.Text -> Docum
 parseToDoc cont = sanEmpty $ go (reverse $ L.lines cont) []
   where go [] doc = doc
-        go (l : ls) doc | isHeadS l = go ls $ (Headl "") : doc
-                        | isParaS l = go ls $ (Parag "") : doc
-                        | null doc = go ls $ (Parag l) : doc
-                        | otherwise = go ls
-                                      ((appendToS (head doc) l) : (tail doc))
+        go (l : ls) doc
+          | isHeadS l = go ls $ (Headl "") : doc
+          | isParaS l = go ls $ (Parag "") : doc
+          | null doc = go ls $ (Parag l) : doc
+          | isAboveHeadS doc = go ls $ (Parag l) : doc
+          | otherwise = go ls ((appendToS (head doc) l) : (tail doc))
         sanEmpty doc = [s | s <- doc, (contentS s) /= ""]
+
 
 appendToS (Headl t) tn = Headl $ L.append tn (apN t)
 appendToS (Parag t) tn = Parag $ L.append tn (apN t)
@@ -58,6 +91,8 @@ isHeadS line | L.length line < 2 = False
 
 isParaS line = line == ""
 
+isAboveHeadS ((Headl t) : doc) = (length $ L.lines t) == 1
+isAboveHeadS _ = False
 
 {-
  TODO
@@ -69,3 +104,27 @@ parseTitle title = Just $ div (toHtml title)
 parseCategories :: L.Text -> Maybe Html
 parseCategories "" = Nothing
 parseCategories cat = Just $ div (toHtml cat)
+
+{-
+ HTML Stuff
+-}
+parseEdit :: [L.Text] -> L.Text
+parseEdit content = renderHtml $ do
+  putHtml $ parseTitle (content !! 0)
+  putHtml $ parseCategories (content !! 1)
+  putHtml $ parseContent (content !! 2)
+  where putHtml (Just h) = h
+        putHtml Nothing = return ()
+
+parseContent :: L.Text -> Maybe Html
+parseContent cont = case docToHtml $ parseToDoc cont of
+  Nothing -> Nothing
+  Just xs -> Just (toHtml $ xs)
+
+docToHtml :: Docum -> Maybe [Html]
+docToHtml [] = Nothing
+docToHtml doc = Just $ map secToHtml doc
+
+secToHtml :: Section -> Html
+secToHtml (Headl t) = div (toHtml t) ! class_ "headline"
+secToHtml (Parag t) = div (toHtml t) ! class_ "parag"
--- View.hs
+++ View.hs
@@ -3,7 +3,7 @@
 module View where
 
 import Prelude hiding (div, head, id)
-import Text.Blaze.Html (preEscapedString, string, stringValue)
+import Text.Blaze.Html (preEscapedString, string, stringValue, toHtml)
 import Text.Blaze.Html5 (Html, (!), docTypeHtml,
                          head, meta, title, link, script,
                          body, div, img, ul, li, a,
@@ -13,48 +13,40 @@ import Text.Blaze.Html5.Attributes (charset,
 
 
 siteHead :: Html
-siteHead = docTypeHtml $ do
-  head $ do
-    title "elfeck"
-    meta ! charset "utf-8"
-    link ! href "/index.css" ! rel "stylesheet" ! type_ "text/css"
-    link ! href "/edit.css" ! rel "stylesheet" ! type_ "text/css"
-    link ! href "/icon.png" ! rel "icon" ! type_ "image/png"
+siteHead = docTypeHtml $ head $ do
+  title "elfeck"
+  meta ! charset "utf-8"
+  link ! href "/index.css" ! rel "stylesheet" ! type_ "text/css"
+  link ! href "/edit.css" ! rel "stylesheet" ! type_ "text/css"
+  link ! href "/icon.png" ! rel "icon" ! type_ "image/png"
 
 siteHeader :: String -> Bool -> Html
 siteHeader headerSvg True = do
   div ! class_ "header" $ do
-    div ! class_ "headercontainer" $ do
-      ul ! class_ "headerleft" $ do
-        headerEntry "elfeck"
-        headerEntry "whyiliketrees"
-    div ! class_ "headercontainer" $ do
-      preEscapedString headerSvg
-    div ! class_ "headercontainer" $ do
-      ul ! class_ "headerright" $ do
-        headerEntry "math and stuff"
-        headerEntry "drivel"
+    div ! class_ "headercontainer" $ ul ! class_ "headerleft" $ do
+      headerEntry "elfeck"
+      headerEntry "whyiliketrees"
+    div ! class_ "headercontainer" $ preEscapedString headerSvg
+    div ! class_ "headercontainer" $ ul ! class_ "headerright" $ do
+      headerEntry "math and stuff"
+      headerEntry "drivel"
 siteHeader headerSvg False = do
   div ! class_ "header" $ do
-    div ! class_ "headercontainer" $ do
-      ul "" ! class_ "headerleft"
-    div ! class_ "headercontainer" $ do
-      preEscapedString headerSvg
-    div ! class_ "headercontainer" $ do
-      ul "" ! class_ "headerright"
+    div ! class_ "headercontainer" $ ul "" ! class_ "headerleft"
+    div ! class_ "headercontainer" $ preEscapedString headerSvg
+    div ! class_ "headercontainer" $ ul "" ! class_ "headerright"
 
 headerEntry :: String -> Html
-headerEntry name = do
-  li ! class_ "headerentry" $ do
-    a (string name) ! href (stringValue ("/" ++ [a | a <- name, a /= ' ']))
-      ! class_ "headerlink"
+headerEntry name =
+  li ! class_ "headerentry" $
+  a (string name) ! href (stringValue ("/" ++ [a | a <- name, a /= ' ']))
+  ! class_ "headerlink"
 
 testBody :: Html
 testBody = div "" ! class_ "testbody"
 
 site404 :: Html
-site404 = do
-  div "Sorry nothing to see here. Try a different URL" ! class_ "testbody"
+site404 = div "Sorry nothing to see here" ! class_ "testbody"
 
 siteEdit :: Html
 siteEdit = do
