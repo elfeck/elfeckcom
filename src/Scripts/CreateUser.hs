cc72c68792c0745f6e0de061a8b0e14d8bb02888
Sebastian Kreisel <kreisel.sebastian@gmail.com>
2015-06-29 10:56:21 +0200

    small fix in CreateUser

--- src/Scripts/CreateUser.hs
+++ src/Scripts/CreateUser.hs
@@ -7,7 +7,7 @@ import Database.Persist
 import Database.Persist.Sqlite
 import qualified Data.Text as T
 
-import Model.Model
+import Model.Types
 
 createUser :: String -> String -> String -> Int -> IO ()
 createUser dbpath name pw access = do
