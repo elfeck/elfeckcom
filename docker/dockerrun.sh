60725f86d45453a333d3a6b728512ae1dd94f01f
Sebastian Kreisel <kreisel.sebastian@gmail.com>
2015-08-06 19:09:56 +0200

    changed dockerrun to accomodate new whyiliketree locations

--- docker/dockerrun.sh
+++ docker/dockerrun.sh
@@ -3,5 +3,5 @@ version=$(cat "dockerversion.txt")
 echo "starting up docker with current version: $version"
 docker run -d 	-v /home/seb/elfeckcom/db:/var/elfeckcom/private/db \
 		-v /home/seb/elfeckcom/config.txt:/var/elfeckcom/private/config.txt \
-		-v /home/seb/whyiliketrees/whyiliketrees.js:/var/elfeckcom/public/static/games/whyiliketrees.js \
+		-v /home/seb/whyiliketrees/js:/var/elfeckcom/public/static/games/whyiliketrees \
 		-p 80:3000 elfeck/elfeckcom:$version
