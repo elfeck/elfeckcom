a6e30d85f427a87399dd2e09749d58c8579e5c2a
Sebastian <kreisel.sebastian@gmail.com>
2016-06-23 13:13:32 +0200

    ssl

--- docker/dockerrun.sh
+++ docker/dockerrun.sh
@@ -1,8 +1,15 @@
 #!/bin/bash
-version=$(cat "dockerversion.txt")
-echo "starting up docker with current version: $version"
+
+version=$(cat "/home/seb/elfeckcom/docker/dockerversion.txt")
+
+echo "Starting up elfeckcom with current version: $version ..."
 docker run -d 	-v /home/seb/elfeckcom/db:/var/elfeckcom/private/db \
 		-v /home/seb/elfeckcom/config.txt:/var/elfeckcom/private/config.txt \
+		-v /home/seb/elfeckcom/log:/var/elfeckcom/private/log \
 		-v /home/seb/whyiliketrees/js:/var/elfeckcom/public/static/games/whyiliketrees \
 		-v /home/seb/LD29/build/web:/var/elfeckcom/public/static/games/LD29 \
-		-p 80:3000 elfeck/elfeckcom:$version
+		--expose=3000 \
+		-e VIRTUAL_HOST=www.elfeck.com,elfeck.com \
+		-e LETSENCRYPT_HOST=elfeck.com,www.elfeck.com \
+                -e LETSENCRYPT_EMAIL=kreisel.sebastian@gmail.com \
+		elfeck/elfeckcom:$version
